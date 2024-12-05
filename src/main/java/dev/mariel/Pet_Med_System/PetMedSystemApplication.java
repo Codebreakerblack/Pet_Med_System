f336ebed00ccba1ebcda1621c6748db3bcdaa37c
Codebreakerblack <blancohmariel@gmail.com>
2024-12-05 16:11:36 +0100

    add proyect spring

---
+++ src/main/java/dev/mariel/Pet_Med_System/PetMedSystemApplication.java
@@ -0,0 +1,13 @@
+package dev.mariel.Pet_Med_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetMedSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetMedSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/mariel/Pet_Med_System/PetMedSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mariel.Pet_Med_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetMedSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
